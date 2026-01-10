b93ea1b69863c59b2263bde55f8fd42d5096a293
Nathan Faucett <nathanfaucett@gmail.com>
2026-01-10 13:32:06 -0500

    fix redirect

--- apps/oidc-admin/src/routes/callback/+page.ts
+++ apps/oidc-admin/src/routes/callback/+page.ts
@@ -2,6 +2,7 @@ import { getUserManager } from '$lib/common/state/user.svelte';
 import { redirect } from '@sveltejs/kit';
 import type { PageLoad } from './$types';
 import { createNotification } from '$lib/common/state/notifications.svelte';
+import { resolve } from '$app/paths';
 
 export const load: PageLoad = async (event) => {
 	await event.parent();
@@ -12,7 +13,7 @@ export const load: PageLoad = async (event) => {
 		if (e instanceof Error) {
 			createNotification(e.message);
 		}
-		redirect(302, '/signin');
+		redirect(302, resolve('/signin'));
 	}
-	redirect(302, '/');
+	redirect(302, resolve('/'));
 };
